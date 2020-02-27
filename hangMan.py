c207fbbab7dd69a0e92a509565a5bbd0810a7ae5
sprass22 <60894823+sprass22@users.noreply.github.com>
2020-02-27 13:34:21 -0500

    Update hangMan.py
    check function added

--- hangMan.py
+++ hangMan.py
@@ -1,31 +1,69 @@
 from __future__ import print_function
 lives = 6
 word = ""
-letter = ""
+letter = " "
 updatedSpaces = []
 def initialize():
     global word
     global updatedSpaces
-    word = "airport"
-    print ("_ " * len(word))
-    updatedSpaces = ("_ " * len(word))
+    print("lets play hangman!!")
+    word = input("type your word here:")
+    lenW = len(word)
     print("try to guess the word in less than 6 tries")
+    while lenW != 0:
+        updatedSpaces.append('-')
+        lenW -=1
     print(updatedSpaces)
 def getLetter():
     global letter
-    print ("What is your guess")
-    letter = raw_input()
-#def check():        
+    letter = input("type your guess here: ")
+    check()
+def check():
+    # checks the guess that the user makes
+    global word
+    global lives
+    global updatedSpaces
+    if letter in word:
+        i = (word.find(letter)) 
+        del updatedSpaces[i]
+        updatedSpaces.insert(i, letter)
+        if i != len(word)-1:
+            e = (word.find(letter, i+1, len(word) -1))
+            if e != -1:
+                del updatedSpaces[e]
+                updatedSpaces.insert(e, letter)
+                if e != len(word)-1:
+                    a = word.find(letter, e+1, len(word)+1)
+                    if a != -1:
+                        del (updatedSpaces[a])
+                        updatedSpaces.insert(a, letter)
+                else:
+                    win()
+        else:
+            win()
+                
+    if letter not in word:
+        print ('Sorry this letter is not in word')
+        lives -= 1
+        print ('You have', lives, 'lives left')
+        getLetter() 
+    print (updatedSpaces[0])
+    print (updatedSpaces)
+    getLetter()
+ 
+     
 def win():
-    if updatedSpaces == word:
-        print("you won the game!")
-    else:
-        if lives == 0:
-            print("you lost")
+    global lives
+    global updatedSpaces
+    print (updatedSpaces)
+    if '-' not in updatedSpaces:
+        print ("CONGRADULATIONS. YOU ARE THE CHAMPION")
+        main()
+
 def main():
     initialize()
     getLetter()
-    #check()
+    check()
     
-     
+main()
     
